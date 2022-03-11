5331ba5ddc27fbbc58a97a0cad078f5069cad3a8
thissideaditya <adityac486@gmail.com>
2022-03-11 13:58:08 +0530

    started wcat and three functions are done.

---
+++ wcat.js
@@ -0,0 +1,36 @@
+// 1) node wcat.js filepath => displays the contents of a file in terminal
+// 2) node wcat.js filepath1 filepath2 filepath3  => displays the content of all files in terminal in concatinated form in given order
+// 3) node wcat.js -n file1 file 2 file3 OR node wcat.js -n file1 
+//node wcat.js f1.txt
+//node wcat.js f1.txt f2.txt f3.txt
+const fs = require("fs");
+// let input = process.argv;
+// console.log(input);
+let inputArr = process.argv.slice(2);
+// console.log(inputArr);
+let filesArr = [];
+let optionsArr = [];
+//===============> placed files path in filesArr <=============
+for (let i = 0; i < inputArr.length; i++){
+    let firstChar = inputArr[i].charAt(0);
+    // console.log(firstChar);
+    if (firstChar == '-') {
+        optionsArr.push(inputArr[i]);
+    }
+    else {
+        filesArr.push(inputArr[i]);
+    }
+}
+// console.log("file to be read are " + filesArr);
+
+//=============>check if all the files are present<============= //
+for (let i = 0; i < filesArr.length; i++){
+    let doesExist = fs.existsSync(filesArr[i]);
+    if (!doesExist) {
+        console.log("One or more File(s) do not exist ");
+        // return;
+        process.exit();
+    }
+}
+
+// =============>content read and appending starts<=============//
