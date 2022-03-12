e6f1340b14502209cc421ccccaf1a0769e3b7b54
thissideaditya <adityac486@gmail.com>
2022-03-12 14:40:38 +0530

    -n -b added succesfully

--- wcat.js
+++ wcat.js
@@ -28,7 +28,7 @@ for (let i = 0; i < filesArr.length; i++){
     let doesExist = fs.existsSync(filesArr[i]);
     if (!doesExist) {
         console.log("One or more File(s) do not exist ");
-        // return;
+        // return; // it also works
         process.exit();
     }
 }
